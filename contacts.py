e6f9e0e7217b22447638287788ee8f3eea5550a8
krithikashree1957 <krithikashr@gmail.com>
2026-07-30 10:48:32 +0530

    Initial file before cloning

---
+++ contacts.py
@@ -0,0 +1,14 @@
+
+contacts={}
+
+def add_contact(name, phone):
+    contacts[name]=phone
+def search_contact(name):
+    return contacts.get(name,"Not found")
+
+if __name__=="__main__":
+    add_contact("Lala","986544987")
+    add_contact("Kiki","125384677")
+    print("Arun's number: ",search_contact("Arun"))
+    print("Search Kabir:", search_contact("Kabir"))
+    
